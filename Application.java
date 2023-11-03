9597b002c0bd5dca1663970b1544f83ae71064f7
erimarko <127380987+erimarko@users.noreply.github.com>
2023-11-03 08:59:11 +0100

    Addit Application to call calcluator

---
+++ Application.java
@@ -0,0 +1,7 @@
+public class Application{
+	public string void main(String[] args){
+		Calculator calculator = new Calculator();
+		System.out.println(calculator.addition(1,2));
+		System.out.println(calculator.addition(1.5,1.3));
+	}
+}
